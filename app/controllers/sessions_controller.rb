c962a7052769e3e19f6315b56a81e244afa1a362
Matias Pizarro <nomorejim@gmail.com>
2012-11-05 01:03:24 -0300

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+	  user = User.find_by_email(params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+	  	flash[:success] = "Welcome again #{user.name}"
+	  	sign_in user
+	    redirect_to user
+	  else
+	    flash.now[:error] = "Invalid email//password combination"
+	    render 'new'
+	  end
 	end
 
 	def destroy
+		sign_out user
+		flash[:success] = "You have been correctly signed out"
+		redirect_to root
 	end
 end
